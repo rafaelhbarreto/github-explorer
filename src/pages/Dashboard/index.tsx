8d30586bc4c6c4daaf03c5b2a73252e78aa70e74
rafael.barreto <rafael.barreto@go.senac.br>
2021-02-03 16:24:30 -0300

    First commit / Finalização da busca e listagem de repositorios

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,91 @@
+import React, { useState, FormEvent } from 'react';
+
+// importação dos icones no react
+import { FiChevronRight } from 'react-icons/fi';
+
+// importação da api
+import api from '../../services/api';
+
+// importação dos arquivos
+import appLogo from '../../assets/images/logo.svg';
+
+// importação dos estilos
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [error, setError] = useState('');
+  const [repostiories, setRepositories] = useState<Repository[]>([]);
+
+  /**
+   * Função para manipulação da ação de adicionar um novo repositório.
+   * @param event
+   */
+  async function handleAddRepositories(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setError('Você deve informar o autor/repositório do github');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+      const repository = response.data;
+      setRepositories([...repostiories, repository]);
+
+      setError('');
+      setNewRepo('');
+    } catch (err) {
+      setError('Não foi possível encontrar o repositório informado.');
+    }
+  }
+
+  return (
+    <>
+      <img src={appLogo} alt="Github explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form hasError={!!error} onSubmit={handleAddRepositories}>
+        <input
+          type="text"
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => {
+            setNewRepo(e.target.value);
+          }}
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Error>{error}</Error>
+
+      <Repositories>
+        {repostiories.map(repository => (
+          <a href="teste" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/services/api.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://api.github.com',
+});
+
+export default api;
